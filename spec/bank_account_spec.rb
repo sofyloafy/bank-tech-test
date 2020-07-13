3c7464d3c89007cfc033dca37efe414a4a089746
Sophie <sophiebrown438@gmail.com>
2020-07-13 10:59:33 +0100

    Rubocop amendments

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/bank_account'
 
-describe  BankAccount do
-  let (:bank_account) { BankAccount.new }
+describe BankAccount do
+  let(:bank_account) { BankAccount.new }
   it 'should allow user to have a bank account' do
     expect(bank_account).to eq
   end
