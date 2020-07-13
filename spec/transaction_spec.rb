c33600828050d7c2ede0a0da8230d115bbc66db5
Sophie <sophiebrown438@gmail.com>
2020-07-13 11:24:14 +0100

    Add Transaction class and spec

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require_relative '../lib/transaction'
+
+describe Transaction do
+  let(:transaction) { Transaction.new }
+  
+  it 'should be able to withdraw' do
+  end
+end 
