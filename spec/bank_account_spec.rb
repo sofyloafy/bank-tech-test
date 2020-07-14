072c65c7644d450c4ee0d18cf4d005ea6d22b255
Sophie <sophiebrown438@gmail.com>
2020-07-14 10:34:13 +0100

    Pass test for deposit in BankAccount class

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 require_relative '../lib/bank_account'
 
 describe BankAccount do
+  let(:transaction) { Transaction.new }
+  let(:statement) { Statement.new }
   let(:bank_account) { BankAccount.new(transaction, statement) }
-  let(:transaction) { double('transaction') }
-  let(:statement) { double('statement') }
 
   it "should be initialized" do
-    allow(bank_account.transaction.deposit(6))
-    expect(bank_account.transaction.deposit(6)).to eq 6
+    expect(bank_account.transaction.deposit(6)).to eq [{:balance=>"£6.00", :credit=>"£6.00", :date=>"14/07/2020", :debit=>"------"}]
   end
 
   it "should reject anything but an integer" do
