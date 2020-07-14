7369fa67b25805efaed3bfa0942a1bb0a179a927
Sophie <sophiebrown438@gmail.com>
2020-07-14 13:59:53 +0100

    Clean rubocop offenses

--- spec/bank_account_feature_spec.rb
+++ spec/bank_account_feature_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/bank_account'
 describe BankAccount do
   let(:account) { BankAccount.new(Transaction.new, Statement.new) }
   let(:date) { Time.now.strftime('%d/%m/%Y') }
-  let(:desired_result){ " Date      || Credit || Debit || Balance \n  #{date} || ------  || £500.00 || £2500.00   #{date} || £2000.00  || ------ || £3000.00   #{date} || £1000.00  || ------ || £1000.00 "}
+  let(:desired_result) { " Date      || Credit || Debit || Balance \n  #{date} || ------  || £500.00 || £2500.00   #{date} || £2000.00  || ------ || £3000.00   #{date} || £1000.00  || ------ || £1000.00 " }
   
   it 'should run a feature test' do
     account.deposit(1000)
